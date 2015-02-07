650446ed09fd513276c5aa0bdc0364c961178ef1
alexzkazu <alex.ziebol@gmail.com>
2015-02-06 22:42:33 -0800

    rough pseudocode

---
+++ README.md
@@ -0,0 +1,2 @@
+# pair-sort
+have a list of names and pair 2 people together for pair programming
---
+++ test/pair.spec.js
@@ -0,0 +1,30 @@
+var expect = require("chai").expect;
+
+// takes a master list of name objects
+
+// each object contains an array of the past partners' names.
+
+// takes a random item in a list, another random item in the list
+
+// compares whether they've been paired in the past before
+
+// if they have, re-run the picking function
+
+// if not, pushes both names into an array
+
+// and pushes each other's names into the objects, contained in the master name list.
+
+
+
+
+// describe("", function() {
+
+//   it("", function() {
+
+
+
+// 	expect(negativeNum).to.be.at.least(0);
+
+//   });
+
+// });
